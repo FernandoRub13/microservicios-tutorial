b0b0f89d9899dc4038b477be2a900a3f974b6da8
Fernando Rubio <pablo.rubio.bailon@gmail.com>
2023-12-10 11:28:07 -0600

    Revert "Cambio en configuracion de los yaml"
    This reverts commit 38f67863d1e1af13f6574e12becd4fd2a15b4ecb.

--- usuario/src/main/java/com/usuario/service/UsuarioApplication.java
+++ usuario/src/main/java/com/usuario/service/UsuarioApplication.java
@@ -2,12 +2,10 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "com.usuario.service.feignclients")
-@EnableDiscoveryClient
 public class UsuarioApplication {
 
 	public static void main(String[] args) {
